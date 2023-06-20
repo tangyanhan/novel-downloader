8224d44e2592709c17b97679c420783a365a9e4c
Ethan Tang <tang_yanhan@126.com>
2023-06-20 15:14:20 +0800

    A simple crawler to get novel in text format

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # novel-downloader
+
+Download novel by crawling on a webpage.
+
+## Example
+
+```
+go run cmd/downloader/main.go -contents http://wap.33yqxs.com/112/112958/all.html -base http://wap.33yqxs.com  -out 国民法医.txt
+```
--- cmd/downloader/main.go
+++ cmd/downloader/main.go
@@ -1,59 +1,181 @@
 package main
 
 import (
-	"encoding/xml"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
+	"fmt"
 	"io"
 	"io/fs"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
-type PageBody struct {
-	XMLName  xml.Name `xml:"body"`
-	Chapters []struct {
-		XMLName xml.Name `xml:"p"`
-	} `xml:"p"`
-	Body struct {
-		Chapters []struct {
-		}
-	} `xml:"body"`
-	// 	<body>
-	// <header id="top" class="channelHea channelHea2">
-	// <a href="javascript:history.go(-1);" class="iconback"><img src="/images/header-back.gif" alt="返回" /></a>
-	// <span class="title">国民法医</span>
-	// <a href="/" class="iconhome"><img src="/images/header-backhome.gif" alt="首页" /></a>
-	// </header>
-	// <div id="chapterlist" class="directoryArea">
-	// <p><a href="#bottom" style="color:Red;">↓直达页面底部↓</a></p>
-	// <p><a href="/112/112958/64643228.html">第一章：十七叔</a></p>
-}
+var client http.Client
 
-func main() {
-	var baseUrl string
-	flag.StringVar(&baseUrl, "url", "", "Url path of novel")
-	flag.Parse()
-
-	client := http.Client{
+func init() {
+	client = http.Client{
 		Transport: &http.Transport{
 			Proxy: nil,
 		},
 	}
+}
 
-	resp, err := client.Get(baseUrl)
+func GetUrlWithCache(url string, fileName string) (filePath string, content []byte, err error) {
+	const cacheFolder = "cache"
+	sum := md5.Sum([]byte(url))
+	dst := make([]byte, hex.EncodedLen(len(sum)))
+	hex.Encode(dst, sum[:])
+	if fileName != "" {
+		filePath = path.Join(cacheFolder, fileName)
+	} else {
+		filePath = path.Join(cacheFolder, string(dst)+".html")
+	}
+
+	if _, err = os.Stat(filePath); err == nil {
+		log.Println("Cache Hit: ", url, "->", filePath)
+		content, err = ioutil.ReadFile(filePath)
+		return
+	}
+
+	if _, err = os.Stat(cacheFolder); err != nil {
+		os.Mkdir(cacheFolder, fs.ModePerm)
+	}
+	var req *http.Request
+	var resp *http.Response
+	req, err = http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatalln(err)
+		return
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+	resp, err = client.Do(req)
+	if err != nil {
+		return
 	}
 	if resp.StatusCode/100 != 2 {
-		log.Fatalln("Status code failed with:", resp.StatusCode)
+		return
 	}
 
-	raw, err := io.ReadAll(resp.Body)
+	content, err = io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln("failed to read body:", err)
+		return
 	}
-	ioutil.WriteFile("dummy.html", raw, fs.ModePerm)
+	if err = ioutil.WriteFile(filePath, content, fs.ModePerm); err != nil {
+		return
+	}
+	return
+}
 
-	xml.Unmarshal(raw)
+func main() {
+	var contentsUrl string
+	var baseUrl string
+	var outFileName string
+	flag.StringVar(&contentsUrl, "contents", "", "Url path of novel contents page")
+	flag.StringVar(&baseUrl, "base", "", "Base url of the novel site to combine chapter links")
+	flag.StringVar(&outFileName, "out", "", "Filename of the output file")
+	flag.Parse()
+
+	filePath, content, err := GetUrlWithCache(contentsUrl, "")
+	if err != nil {
+		log.Fatalln("Failed to get page", contentsUrl, ":", err)
+	}
+
+	log.Println(contentsUrl, "->", filePath)
+
+	reader := bytes.NewReader(content)
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+	// Find the review items
+	doc.Find("#chapterlist a").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title
+		log.Println("Found chapter=", s.Text())
+		link, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+		chapterName := s.Text()
+		if match, err := regexp.MatchString("第.*章", chapterName); !match || err != nil {
+			log.Println("Cannot match:", chapterName)
+			return
+		}
+
+		buf.WriteString("\n\n=======================\n")
+		buf.WriteString(chapterName)
+		buf.WriteString("\n=======================\n\n")
+
+		if err := DownloadChapter(baseUrl, link, chapterName, 0, &buf); err != nil {
+			log.Fatalln("Failed to download chapter:", err)
+			return
+		}
+	})
+
+	if err := ioutil.WriteFile(outFileName, buf.Bytes(), os.ModePerm); err != nil {
+		log.Fatalln("Failed to write file to", outFileName, "Error:", err)
+	}
+}
+
+func DownloadChapter(baseUrl, link, chapterName string, page int, buf *bytes.Buffer) error {
+	fmt.Println("Chapter=", chapterName, "Url=", link)
+	chapterURL := baseUrl + link
+	filePath, content, err := GetUrlWithCache(chapterURL, fmt.Sprintf("%s-%d.html", chapterName, page))
+	if err != nil {
+		log.Fatalln("Failed to get page", chapterURL, ":", err)
+	}
+	log.Println(chapterName, "->", filePath)
+
+	reader := bytes.NewReader(content)
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chapterContent := doc.Find("#chaptercontent")
+	if chapterContent == nil {
+		return nil
+	}
+	rootDiv := chapterContent.Nodes[0]
+	var hasLineEnd bool
+	for node := rootDiv.FirstChild; node != nil; node = node.NextSibling {
+		switch node.Type {
+		case html.TextNode:
+			buf.WriteString(strings.TrimSpace(node.Data))
+			hasLineEnd = false
+		case html.ElementNode:
+			if node.Data == "br" && !hasLineEnd {
+				buf.WriteString("\n")
+				hasLineEnd = true
+			}
+		default:
+		}
+	}
+
+	// Find the review items
+	nextPage := doc.Find("#pt_next")
+	if nextPage == nil {
+		return nil
+	}
+	nextPageUrl, ok := nextPage.Attr("href")
+	if !ok {
+		log.Println("No next page link for ", chapterName, filePath)
+		return nil
+	}
+
+	if match, err := regexp.MatchString("_\\d+.html", nextPageUrl); !match || err != nil {
+		return nil
+	}
+
+	return DownloadChapter(baseUrl, nextPageUrl, chapterName, page+1, buf)
 }
